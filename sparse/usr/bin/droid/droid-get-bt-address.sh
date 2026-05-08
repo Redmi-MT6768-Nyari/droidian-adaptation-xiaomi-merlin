bf96a5ae384866d85b49c32a3d88cabad972686a
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2026-05-08 19:23:44 +0700

    droidian: Jbub5
    Unmodified files & Sources:
    /etc/phosh/phoc.ini https://raw.githubusercontent.com/droidian-devices/adaptation-droidian-miatoll/db4865cebeb2e3ab38fa825bcda57d7f4c068e65/etc/phosh/phoc.ini
    /etc/pulse/arm_droid_card_custom.pa https://raw.githubusercontent.com/droidian-devices/adaptation-volla-mimir/06084d5d21b87ee98db04ae08fa135228c1ca1f0/sparse/etc/pulse/arm_droid_card_custom.pa
    /etc/udev/rules.d/70-no-ap0.rules https://raw.githubusercontent.com/droidian-devices/adaptation-volla-mimameid/e344ca5668b998a89a03e6d0758472cffe1875a0/etc/udev/rules.d/70-no-ap0.rules
    /etc/udev/rules.d/70-no-wlan1.rules https://raw.githubusercontent.com/droidian-devices/adaptation-volla-mimameid/8f99ad7df9366dadb08afb8a7ae3f45a2a469f11/etc/udev/rules.d/70-no-wlan1.rules
    /usr/lib/systemd/system/plasma-mobile-wf.service.d/90-lights.conf https://raw.githubusercontent.com/droidian-devices/adaptation-volla-mimir/06084d5d21b87ee98db04ae08fa135228c1ca1f0/sparse/usr/lib/systemd/system/plasma-mobile-wf.service.d/90-lights.conf
    
    Have been modified & Sources(original file):
    /etc/ofono/binder.d/11-radio.conf https://raw.githubusercontent.com/droidian-devices/adaptation-droidian-miatoll/f9d7a7a0889472e62ac9bc876df76a33b4a73d42/etc/ofono/binder.d/11-radio.conf
    /usr/bin/droid/droid-get-bt-address.sh https://raw.githubusercontent.com/droidian-devices/adaptation-droidian-angelica/34688b2c472c937e733dafcf71b0bc6ec05cbbd2/usr/bin/droid/droid-get-bt-address.sh

--- sparse/usr/bin/droid/droid-get-bt-address.sh
+++ sparse/usr/bin/droid/droid-get-bt-address.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-
-cd /var/lib/bluetooth
-find . -mindepth 1 -maxdepth 1 -type d | cut -c3- | awk -F: -vOFS=: '{print $6,$5,$4,$3,$2,$1}' > /var/lib/bluetooth/board-address
-chown root:root /var/lib/bluetooth/board-address
-chmod 644 /var/lib/bluetooth/board-address
-exit 0
+[ -f /var/lib/bluetooth/board-address ] && exit 0
+find /var/lib/bluetooth -mindepth 1 -maxdepth 1 -type d  -exec basename {} \; | awk -F: -vOFS=: '{print $6,$5,$4,$3,$2,$1}' > /var/lib/bluetooth/board-address
